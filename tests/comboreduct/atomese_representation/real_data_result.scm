1d2eda5ab8a8185b0c33d2a5c279ed968b4abbec
Masresha <masreshabeniam@gmail.com>
2018-06-07 20:42:57 +0300

    add unit tests for atomese representation

---
+++ tests/comboreduct/atomese_representation/real_data_result.scm
@@ -0,0 +1,8 @@
+(define real_data_repr
+ (Similarity (stv 1 1)
+  (List (Schema "i1") (Schema "i2") (Schema "o"))
+  (Set
+   (List (Node "r1") (List (Number 0) (Number 2) (Number 2)))
+   (List (Node "r2") (List (Number 1) (Number 0) (Number 1)))
+   (List (Node "r3") (List (Number 0) (Number 0) (Number 0)))))
+)
