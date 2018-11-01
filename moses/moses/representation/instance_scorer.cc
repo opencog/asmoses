d12977ee48001e1beb9f2d681f9de08a2a8913b0
behailu <behailu04@gmail.com>
2018-11-01 13:38:22 +0300

    Change indentation to tab

--- moses/moses/representation/instance_scorer.cc
+++ moses/moses/representation/instance_scorer.cc
@@ -78,7 +78,7 @@ composite_score atomese_based_scorer::operator()(const instance &inst) const
 	combo_tree tr = _rep.get_candidate(inst, _reduce);
 	Handle handle = atomese_combo(tr);
 	if(_as) {
-        _as->add_atom(handle);
+		_as->add_atom(handle);
 	}
 	return _cscorer.get_cscore(handle);
 }
