ed76fbbbf90b766a287837b45374e4e139a60986
kasim <se.kasim.ebrahim@gmail.com>
2019-02-24 17:11:02 +0300

    Add reduct_to_link

---
+++ opencog/reductURE/types/ReductTypes.cc
@@ -0,0 +1,14 @@
+/**
+ * ReductTypes.cc
+ *
+ * Atom Types used in reduct.
+ *
+ * Copyright (c) 2019 Kasim
+ */
+
+#include "opencog/reductURE/types/atom_types.definitions"
+
+#define INHERITANCE_FILE "opencog/reductURE/types/atom_types.inheritance"
+#define INITNAME reduct_types_init
+
+#include <opencog/atoms/atom_types/atom_types.cc>
