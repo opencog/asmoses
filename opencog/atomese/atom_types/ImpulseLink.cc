c1a0167fd0cff79848c3e50f9833430f0013ff62
Yidnekachew <searchyidne@gmail.com>
2019-07-04 22:49:53 +0900

    initial impulselink

---
+++ opencog/atomese/atom_types/ImpulseLink.cc
@@ -0,0 +1,60 @@
+/*
+ * asmoses/opencog/atomese/atom_types/ImpulseLink.cc
+ *
+ * Copyright (C) 2019 Yidnekachew W.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the
+ * exceptions at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <opencog/atoms/base/ClassServer.h>
+#include <opencog/atoms/execution/EvaluationLink.h>
+#include <opencog/atoms/core/NumberNode.h>
+
+#include "ImpulseLink.h"
+
+using namespace opencog;
+
+ImpulseLink::ImpulseLink(const HandleSeq& oset, Type t)
+		: FunctionLink(oset, t)
+{
+	// Type must be as expected
+	if (not nameserver().isA(t, IMPULSE_LINK)) {
+		const std::string& tname = nameserver().getTypeName(t);
+		throw SyntaxException(TRACE_INFO,
+		                      "Expecting a ImpulseLink, got %s", tname.c_str());
+	}
+
+	if (1 != oset.size())
+		throw SyntaxException(TRACE_INFO,
+		                      "ImpulseLink expects only one argument.");
+
+	Type tf = oset[0]->get_type();
+	if (nameserver().isA(tf, BOOLEAN_LINK))
+		throw SyntaxException(TRACE_INFO,
+		                      "Expecting a boolean input.");
+}
+
+ValuePtr ImpulseLink::execute(AtomSpace* scratch, bool silent)
+{
+	TruthValuePtr tvp(EvaluationLink::do_evaluate(scratch, _outgoing.at(0)));
+
+	if (tvp->get_mean() > 0.5)
+		return createNumberNode(1);
+	else
+		return createNumberNode(0);
+}
+
+DEFINE_LINK_FACTORY(ImpulseLink, IMPULSE_LINK)
---
+++ opencog/atomese/atom_types/ImpulseLink.h
@@ -0,0 +1,59 @@
+/*
+ * opencog/atoms/core/ImpulseLink.h
+ *
+ * Copyright (C) 2019 Yidnekachew W.
+ *
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef ASMOSES_IMPULSELINK_H
+#define ASMOSES_IMPULSELINK_H
+
+#include <opencog/atoms/core/FunctionLink.h>
+#include <opencog/atoms/core/ScopeLink.h>
+#include <opencog/atoms/core/Quotation.h>
+
+namespace opencog
+{
+class ImpulseLink : public FunctionLink
+{
+public:
+	ImpulseLink(const HandleSeq&, Type= IMPULSE_LINK);
+
+	virtual ValuePtr execute(AtomSpace*, bool);
+
+	static Handle factory(const Handle&);
+};
+
+typedef std::shared_ptr<ImpulseLink> ImpulseLinkPtr;
+
+static inline ImpulseLinkPtr ImpulseLinkCast(const Handle& h)
+{
+	AtomPtr a(h);
+	return std::dynamic_pointer_cast<ImpulseLink>(a);
+}
+
+static inline ImpulseLinkPtr ImpulseLinkCast(AtomPtr a)
+{ return std::dynamic_pointer_cast<ImpulseLink>(a); }
+
+#define createImpulseLink std::make_shared<ImpulseLink>
+
+/** @}*/
+}
+
+#endif //ASMOSES_IMPULSELINK_H
