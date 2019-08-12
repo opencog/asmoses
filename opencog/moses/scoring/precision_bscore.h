2dc63137d805d92632bb8f81b9db152ff09976fb
Bitseat <bitseatt@gmail.com>
2019-08-12 18:00:49 +0300

    Port precision_bscore

--- opencog/moses/scoring/precision_bscore.h
+++ opencog/moses/scoring/precision_bscore.h
@@ -123,6 +123,9 @@ struct precision_bscore : public bscore_ctable_time_dispersion
     behavioral_score operator()(const scored_combo_tree_set&) const;
     score_t get_error(const combo_tree&) const;
 
+	behavioral_score operator()(const Handle& handle) const;
+	score_t get_error(const Handle&) const;
+
     // Return the best possible bscore. Used as one of the
     // termination conditions (when the best bscore is reached).
     behavioral_score best_possible_bscore() const;
