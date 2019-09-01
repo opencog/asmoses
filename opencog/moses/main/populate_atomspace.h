fbde0f038b1081e8c56b5976cd039dfcbc400d14
Eman <emanwel2@gmail.com>
2019-09-01 16:18:14 +0300

    Added function that populates a Node's values for UTests

--- opencog/moses/main/populate_atomspace.h
+++ opencog/moses/main/populate_atomspace.h
@@ -29,6 +29,7 @@
 #include <opencog/atomspace/AtomSpace.h>
 #include <opencog/utils/value_key.h>
 #include <opencog/atoms/base/Link.h>
+#include <opencog/utils/valueUtils.h>
 
 namespace opencog { namespace moses {
 using namespace combo;
@@ -56,6 +57,66 @@ void populate(AtomSpace *as, const CTable &ctable);
  * @return ValuePtr containing values such as FloatValue, LinKValues...etc
  */
 ValuePtr vertex_seq_to_value(const vertex_seq& vseq, id::type_node tnode);
+#define GET_ENUM [](const vertex &ver) { \
+    auto en = get_enum_type(ver); \
+    return ValuePtr(createNode(CONCEPT_NODE, en.getContent())); \
+    }
+		
+template <typename... HandleArgs>
+inline void populate_frm_ctable(CTable _ctable, HandleArgs &&... handleArgs) {
+	std::vector<Handle> vect1{std::forward<HandleArgs>(handleArgs)...};
+	int index = 0;
+	for (Handle h: vect1) {
+		Type type = h->get_type();
+		ValueSeq _f_values;
+		if (type == PREDICATE_NODE) {
+			boost::transform(_ctable.get_input_col_data(index), back_inserter(_f_values), bool_vertex_to_value);
+			ValuePtr f_proto_atom(new LinkValue(_f_values));
+			h->setValue(moses::compressed_value_key, f_proto_atom);
+		} else if (type == CONCEPT_NODE) {
+			ValueSeq _f_values;
+			boost::transform(_ctable.get_input_col_data(index), back_inserter(_f_values), GET_ENUM);
+			ValuePtr f_proto_atom(new LinkValue(_f_values));
+			h->setValue(moses::compressed_value_key, f_proto_atom);
+		} else if (type == SCHEMA_NODE) {
+			std::vector<double> _f_values;
+			boost::transform(_ctable.get_input_col_data(index), back_inserter(_f_values), get_contin);
+			ValuePtr f_proto_atom(new FloatValue(_f_values));
+			h->setValue(moses::compressed_value_key, f_proto_atom);
+		} else OC_ASSERT(false, "Node Type don't exist or not implemented!");
+		index++;
+	}
+}
+
+template <typename... HandleArgs>
+inline void populate_frm_table(Table _table, HandleArgs &&... handleArgs) {
+	std::vector<Handle> vect1{std::forward<HandleArgs>(handleArgs)...};
+	int index = 0;
+	for (Handle h: vect1) {
+	Type type = h->get_type();
+	ValueSeq _f_values;
+	if (type == PREDICATE_NODE) {
+		boost::transform(_table.itable.get_column_data(index), back_inserter(_f_values),
+		                 bool_vertex_to_value);
+		ValuePtr f_proto_atom(new LinkValue(_f_values));
+		h->setValue(moses::value_key, f_proto_atom);
+	} else if (type == CONCEPT_NODE) {
+		ValueSeq _f_values;
+		boost::transform(_table.itable.get_column_data(index), back_inserter(_f_values), GET_ENUM);
+		ValuePtr f_proto_atom(new LinkValue(_f_values));
+		h->setValue(moses::value_key, f_proto_atom);
+	} else if (type == SCHEMA_NODE) {
+		std::vector<double> _f_values;
+		boost::transform(_table.itable.get_column_data(index), back_inserter(_f_values), get_contin);
+		ValuePtr f_proto_atom(new FloatValue(_f_values));
+		h->setValue(moses::value_key, f_proto_atom);
+	} else OC_ASSERT(false, "Node Type don't exist or not implemented!");
+	index++;
+	}
+}
+
+HandleSeq populate(const CTable& ctable);
+
 
 } // ~namespace moses
 } // ~namespace opencog
