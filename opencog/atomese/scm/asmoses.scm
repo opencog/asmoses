f12f85b47dd37e1998f36a5a75da20d0d369da81
Eman22S <emanawel2@gmail.com>
2019-12-04 10:11:04 +0300

    Configured cmake files to make impulselink useable

--- opencog/atomese/scm/asmoses.scm
+++ opencog/atomese/scm/asmoses.scm
@@ -2,10 +2,13 @@
 
 (define-module (opencog asmoses)
   #:use-module (opencog)
+  #:use-module (opencog asmoses-config)
 )
 
+
 ; Load the C library; this calls the nameserver to load the types.
-(load-extension "libasmoses_types" "asmoses_types_init")
+(load-extension (string-append atom_types_path "libasmoses_types") "asmoses_types_init")
 
 ; Load various parts....
-(load "asmoses/types/atom_types.scm")
+(load-from-path  "opencog/asmoses/types/atom_types.scm")
+(export)
