dffc5d0aac7aa6e630d204dd7a962f7fab570ab3
Eman22S <emanawel2@gmail.com>
2020-05-17 04:31:48 +0300

    Renamed reductURE to reducture

--- opencog/reducture/types/ReductTypes.cc
+++ opencog/reducture/types/ReductTypes.cc
@@ -8,7 +8,7 @@
 
 #include "opencog/reductURE/types/atom_types.definitions"
 
-#define INHERITANCE_FILE "opencog/reductURE/types/atom_types.inheritance"
+#define INHERITANCE_FILE "opencog/reducture/types/atom_types.inheritance"
 #define INITNAME reduct_types_init
 
 #include <opencog/atoms/atom_types/atom_types.cc>
